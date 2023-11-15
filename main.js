1a5d7caecc41e6b8085183b6c299d3ae4e456152
Eugen <epopescuu@yahoo.it>
2023-11-15 18:59:46 +0100

    feat: add v-model & v-bind

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const { createApp } = Vue
 const options = {
     data: function() {
         return {
-            message: 'Hello VUE!',
+            message: 'Hello VUEer',
+            name: 'Eugen',
+            mySrc: './hq720.jpg'
         }
     }
 };
 
+// il metodo mount() seve per connettere l'istanza al tag HTML con id #app e renderizzare l'app
 createApp(options).mount('#app')
